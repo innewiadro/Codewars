4d1a4eddf0926e8498b2b063b9393db7d10a9d3c
Michal <innewiadro@gmail.com>
2023-02-26 13:43:55 +0100

    add katas level 7:  The highest profit wins!  Regex validate PIN code  Get the Middle Character add kata level 8:  Sum of positive

--- kata_level8/Fake_binary/Fake_binary.py
+++ kata_level8/Fake_binary/Fake_binary.py
@@ -9,12 +9,3 @@ def fake_bin(x):
 
     return "".join(list_of_strings)
 
-def fake_bin_list_comprehension(x):
-    list_of_strings = list(x)
-    print([list_of_strings[i] if for i in range(len(x)) if int(list_of_strings[i]) >= 5 else list_of_strings[i] ])
-
-    return
-
-if __name__ == "__main__":
-    print(fake_bin("456123"))
-    print(fake_bin_list_comprehension("456123"))
