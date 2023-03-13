d1de0d18b9c03ebbd9f34f416124751b1f26e7e8
Michal <innewiadro@gmail.com>
2023-03-13 21:33:26 +0100

    add katas lev 7 Find the divisors!

--- kata_level7/Find _the_divisors/Find_the_divisior.py
+++ kata_level7/Find _the_divisors/Find_the_divisior.py
@@ -1,8 +1,3 @@
 def divisors(integer):
-    list_of_div = [i for i in range(2,integer) if integer % i ==0]
+    list_of_div = [i for i in range(2,integer) if integer % i == 0]
     return list_of_div if len(list_of_div) != 0 else f'{integer} is prime'
-
-if __name__ == '__main__':
-    print(divisors(8))
-    print(divisors(29))
-    
