d9545f4709206ee713d610fae55bdf202cd55dfe
Michal <innewiadro@gmail.com>
2023-03-16 19:21:13 +0100

    add katas lev 6 Split Strings

--- kata_level6/Split_strings/Split_strings.py
+++ kata_level6/Split_strings/Split_strings.py
@@ -3,10 +3,7 @@ def solution(s):
     list_of_letter = list(s)
     if len(list_of_letter) % 2 != 0:
         list_of_letter.append("_")
-    for i in range(0,len(list_of_letter),2):
+    for i in range(0, len(list_of_letter), 2):
         results.append(''.join(list_of_letter[i:i+2]))
         
     return results
-
-if __name__ == '__main__':
-    print(solution('abcdefg'))
