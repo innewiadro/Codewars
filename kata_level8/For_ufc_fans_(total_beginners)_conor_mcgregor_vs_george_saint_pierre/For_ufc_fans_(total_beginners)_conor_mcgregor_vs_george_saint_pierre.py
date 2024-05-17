9e3c40a9e484bb6e04e2c6c0451f27803c753e90
Michal <innewiadro@gmail.com>
2024-05-17 21:25:44 +0200

    add kata lev 8 For UFC Fans (Total Beginners): Conor McGregor vs George Saint Pierre

---
+++ kata_level8/For_ufc_fans_(total_beginners)_conor_mcgregor_vs_george_saint_pierre/For_ufc_fans_(total_beginners)_conor_mcgregor_vs_george_saint_pierre.py
@@ -0,0 +1,4 @@
+def quote(fighter):
+    if fighter.lower() == "george saint pierre":
+        return "I am not impressed by your performance."
+    return "I'd like to take this chance to apologize.. To absolutely NOBODY!"
