bca45bbcdb6da986b4abfdd9a55c1f0e82aad34e
Michal <innewiadro@gmail.com>
2024-11-09 08:30:48 +0100

    add kata 7 Fix My Phone Numbers!

---
+++ kata_level7/Fix_my_phone_numbers/Fix_my_phone_numbers.py
@@ -0,0 +1,10 @@
+import re
+
+
+def is_it_a_num(s: str) -> str:
+    digits = re.sub(r'\D', '', s)
+
+    if len(digits) == 11 and digits.startswith('0'):
+        return digits
+    else:
+        return "Not a phone number"
