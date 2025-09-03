976cb90eeaf1dad43ebbeb71978b495c97d84526
Michal <innewiadro@gmail.com>
2025-09-03 23:52:37 +0200

    add kata 7 Test Your Knowledge Of Function Scope

---
+++ kata_level7/Reverseit/Reverseit.py
@@ -0,0 +1,11 @@
+def reverse_it(data):
+    if isinstance(data, bool):
+        return data
+    elif isinstance(data, str):
+        return data[::-1]
+    elif isinstance(data, int):
+        return int(str(data)[::-1])
+    elif isinstance(data, float):
+        return float(str(data)[::-1])
+    else:
+        return data
