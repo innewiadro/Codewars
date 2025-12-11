7d763fecd87317fe94bbb6a522210e3deed7923e
Michal <innewiadro@gmail.com>
2025-12-11 22:40:53 +0100

    add kata 7 Capitals first!

--- kata_level7/Capitals_first/Capitals_first.py
+++ kata_level7/Capitals_first/Capitals_first.py
@@ -1,1 +1,12 @@
-
+def capitals_first(text):
+    words = text.split()
+    upper = []
+    lower = []
+    for w in words:
+        if not w[0].isalpha():
+            continue
+        if w[0].isupper():
+            upper.append(w)
+        else:
+            lower.append(w)
+    return " ".join(upper + lower)
