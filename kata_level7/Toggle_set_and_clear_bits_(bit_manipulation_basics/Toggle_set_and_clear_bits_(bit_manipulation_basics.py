892500dfafc358f4b139de78ae33b72616bef493
Michal <innewiadro@gmail.com>
2026-02-18 21:55:11 +0100

    add kata 7 kyu Toggle, Set, and Clear Bits (Bit Manipulation Basics)

---
+++ kata_level7/Toggle_set_and_clear_bits_(bit_manipulation_basics/Toggle_set_and_clear_bits_(bit_manipulation_basics.py
@@ -0,0 +1,26 @@
+def toggle_bit(n, p):
+    return n ^ (1 << p)
+
+
+def set_bit(n, p):
+    return n | (1 << p)
+
+
+def clear_bit(n, p):
+    return n & ~(1 << p)
+
+
+def is_bit_set(n, p):
+    return (n & (1 << p)) != 0
+
+
+def set_multiple_bits(n, mask):
+    return n | mask
+
+
+def clear_multiple_bits(n, mask):
+    return n & ~mask
+
+
+def toggle_multiple_bits(n, mask):
+    return n ^ mask
