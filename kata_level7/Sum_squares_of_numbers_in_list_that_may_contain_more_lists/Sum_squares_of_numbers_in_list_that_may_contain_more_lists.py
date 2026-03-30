56ba06dcedd085200880396e03f63d8d602f9506
Michal <innewiadro@gmail.com>
2026-03-30 19:12:22 +0200

    add kata 7 kyu Sum squares of numbers in list that may contain more lists

---
+++ kata_level7/Sum_squares_of_numbers_in_list_that_may_contain_more_lists/Sum_squares_of_numbers_in_list_that_may_contain_more_lists.py
@@ -0,0 +1,8 @@
+def sumsquares(lst):
+    total = 0
+    for item in lst:
+        if isinstance(item, list):
+            total += sumsquares(item) 
+        else:
+            total += item ** 2
+    return total
